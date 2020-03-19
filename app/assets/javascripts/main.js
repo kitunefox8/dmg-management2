c3392af0781998638ff5c62f70081b558f57a071
TAKAHIRO IWATA <msm_monkey@icloud.com>
2020-03-19 15:37:01 +0900

    js-edit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,11 @@
 // 以下失敗したコードだが軌跡として残す
 // $(document).on('ready page:load', function(){
 // $(document).on('pagecontainerbeforechange', function(){
-$(document).on('click', '.life_button', function(){
 // $('.life_button').on('click', function(){
+$(document).on('click', '.life-minus_button', function(){
   var input = $('.life').val();
   var min = 1;
-  var anser =  input - min 
+  var anser =  input - min;
   if(input >= 1){
     $('.life').val(anser);
   }
@@ -15,24 +15,50 @@ $(document).on('click', '.life_button', function(){
   }
   return false
 });
-
-$(document).on('click', '.move_button', function(){
-    var input = $('.move').val();
-    var min = 1;
-    var anser =  input - min 
-    if(input >= 1){
-      $('.move').val(anser);
-    }
-    else{
-      window.alert('0以下には出来ません');
-    }
-    return false
+$(document).on('click', '.life-plus_button', function(){
+  var input = $('.life').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.life').val(up);
+  }
+  else{
+    $('.life').val(anser);
+  }
+  return false
 });
 
-$(document).on('click', '.attack_button', function(){
+$(document).on('click', '.move-minus_button', function(){
+  var input = $('.move').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.move').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.move-plus_button', function(){
+  var input = $('.move').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.move').val(up);
+  }
+  else{
+    $('.move').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.attack-minus_button', function(){
   var input = $('.attack').val();
   var min = 1;
-  var anser =  input - min 
+  var anser =  input - min;
   if(input >= 1){
     $('.attack').val(anser);
   }
@@ -41,16 +67,42 @@ $(document).on('click', '.attack_button', function(){
   }
   return false
 });
+$(document).on('click', '.attack-plus_button', function(){
+  var input = $('.attack').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.attack').val(up);
+  }
+  else{
+    $('.attack').val(anser);
+  }
+  return false
+});
 
-$(document).on('click', '.range_button', function(){
+$(document).on('click', '.range-minus_button', function(){
   var input = $('.range').val();
   var min = 1;
-  var anser =  input - min 
+  var anser =  input - min;
   if(input >= 1){
     $('.range').val(anser);
   }
   else{
     window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.range-plus_button', function(){
+  var input = $('.range').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.range').val(up);
+  }
+  else{
+    $('.range').val(anser);
   }
   return false
 });
--- app/assets/javascripts/status.js
+++ app/assets/javascripts/status.js
@@ -13,10 +13,15 @@ $(document).on('click', '.poison-minus_button', function(){
 $(document).on('click', '.poison-plus_button', function(){
   var input = $('.poison').val();
   var up = 1;
-  var input = parseInt(input);
-  var anser = input + up;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
   // console.log(anser);
-  $('.poison').val(anser);
+  if(input == ""){
+    $('.poison').val(up);
+  }
+  else{
+    $('.poison').val(anser);
+  }
   return false
 });
 
@@ -35,9 +40,14 @@ $(document).on('click', '.blood-minus_button', function(){
 $(document).on('click', '.blood-plus_button', function(){
   var input = $('.blood').val();
   var up = 1;
-  var input = parseInt(input);
-  var anser = input + up;
-  $('.blood').val(anser);
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.blood').val(up);
+  }
+  else{
+    $('.blood').val(anser);
+  }
   return false
 });
 
@@ -56,9 +66,14 @@ $(document).on('click', '.foot-minus_button', function(){
 $(document).on('click', '.foot-plus_button', function(){
   var input = $('.foot').val();
   var up = 1;
-  var input = parseInt(input);
-  var anser = input + up;
-  $('.foot').val(anser);
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.foot').val(up);
+  }
+  else{
+    $('.foot').val(anser);
+  }
   return false
 });
 
@@ -77,8 +92,169 @@ $(document).on('click', '.arms-minus_button', function(){
 $(document).on('click', '.arms-plus_button', function(){
   var input = $('.arms').val();
   var up = 1;
-  var input = parseInt(input);
-  var anser = input + up;
-  $('.arms').val(anser);
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.arms').val(up);
+  }
+  else{
+    $('.arms').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.faint-minus_button', function(){
+  var input = $('.faint').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.faint').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.faint-plus_button', function(){
+  var input = $('.faint').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.faint').val(up);
+  }
+  else{
+    $('.faint').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.confusion-minus_button', function(){
+  var input = $('.confusion').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.confusion').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.confusion-plus_button', function(){
+  var input = $('.confusion').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.confusion').val(up);
+  }
+  else{
+    $('.confusion').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.curse-minus_button', function(){
+  var input = $('.curse').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.curse').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.curse-plus_button', function(){
+  var input = $('.curse').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.curse').val(up);
+  }
+  else{
+    $('.curse').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.invisible-minus_button', function(){
+  var input = $('.invisible').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.invisible').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.invisible-plus_button', function(){
+  var input = $('.invisible').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.invisible').val(up);
+  }
+  else{
+    $('.invisible').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.buff-minus_button', function(){
+  var input = $('.buff').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.buff').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.buff-plus_button', function(){
+  var input = $('.buff').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.buff').val(up);
+  }
+  else{
+    $('.buff').val(anser);
+  }
+  return false
+});
+
+$(document).on('click', '.blessing-minus_button', function(){
+  var input = $('.blessing').val();
+  var min = 1;
+  var anser =  input - min;
+  if(input >= 1){
+    $('.blessing').val(anser);
+  }
+  else{
+    window.alert('0以下には出来ません');
+  }
+  return false
+});
+$(document).on('click', '.blessing-plus_button', function(){
+  var input = $('.blessing').val();
+  var up = 1;
+  var input_int = parseInt(input);
+  var anser = input_int + up;
+  if(input == ""){
+    $('.blessing').val(up);
+  }
+  else{
+    $('.blessing').val(anser);
+  }
   return false
 });
